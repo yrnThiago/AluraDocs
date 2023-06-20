9a65c22a0b95412694f682686c8bfdd0123c859e
yrnThiago <thiagodias_123@hotmail.com>
2023-06-20 12:18:35 -0300

    feat: add server/client emits

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,14 +1,49 @@
 import io from "./servidor.js";
 
+const documentos = [
+    {
+        nome: "JavaScript",
+        texto: "texto de javascript..."
+    },
+    {
+        nome: "Node",
+        texto: "texto de Node..."
+    },
+    {
+        nome: "Socket.io",
+        texto: "texto de socket.io..."
+    },
+];
+
 io.on("connection", (socket) => {
     console.log("Um cliente se conectou! ID:" , socket.id);
 
-    socket.on("selecionar_documento", (nomeDocumento) => {
+    socket.on("selecionar_documento", (nomeDocumento, devolverTexto) => {
         socket.join(nomeDocumento);
+
+        const documento = encontrarDocumento(nomeDocumento);
+
+        if(documento) {
+            devolverTexto(documento.texto);
+        };
     });
 
     socket.on("texto_editor", ({ texto, nomeDocumento }) => {
-        socket.to(nomeDocumento).emit("texto_editor_clientes", texto)
+        const documento = encontrarDocumento(nomeDocumento);
+
+        if(documento){
+            documento.texto = texto;
+
+            socket.to(nomeDocumento).emit("texto_editor_clientes", texto)
+        }
     });
 
 });
+
+function encontrarDocumento(nome) {
+    const documento = documentos.find((documento) => {
+        return documento.nome === nome;
+    });
+
+    return documento;
+}
